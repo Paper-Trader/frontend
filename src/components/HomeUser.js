55a6bc65faacae9d258e0a55283165f1a016874e
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2020-01-02 13:29:03 -0500

    Basic Components

---
+++ src/components/HomeUser.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomeUser() {
+  return (
+    <div>
+      Home Logged In Page
+    </div>
+  );
+}
+
+export default HomeUser;
---
+++ src/components/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SignIn() {
+  return (
+    <div>
+      Sign In Page
+    </div>
+  );
+}
+
+export default SignIn;
