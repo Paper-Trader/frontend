19074d7c29921e850c843693f60ac3cc7447b090
Kristin Barr <kristinbarr@gmail.com>
2020-01-03 14:20:35 -0500

    sidenav style and icons

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from 'semantic-ui-react';
-import logo from '../assets/logo.svg'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Image } from "semantic-ui-react";
+import dashboardIcon from "../assets/activity-icon.svg";
+import watchListIcon from "../assets/bar-chart-icon.svg";
+import browseIcon from "../assets/eye-icon.svg";
 
 function SideNav() {
   return (
-    <div className="side-nav" >
-      <Link to="/">DASHBOARD</Link>
-      <Link to="/watchlist">WATCHLIST</Link>
-      <Link to="/browse">BROWSE</Link>
+    <div className="side-nav">
+      <div className="sidenav-item sidenav-dashboard">
+        <Image src={dashboardIcon} size="mini" spaced="right" />
+        <Link to="/" className="sidenav-link">
+          DASHBOARD
+        </Link>
+      </div>
+      <div className="sidenav-item nav-watchlist">
+        <Image src={watchListIcon} size="mini" spaced="right" />
+        <Link to="/watchlist" className="sidenav-link">
+          WATCHLIST
+        </Link>
+      </div>
+      <div className="sidenav-item nav-browse">
+        <Image src={browseIcon} size="mini" spaced="right" />
+        <Link to="/browse" className="sidenav-link">
+          BROWSE
+        </Link>
+      </div>
     </div>
   );
 }
