c2b3b9f4e7d06dbc7b355da3b8abd1e939c652e2
Kristin Barr <kristinbarr@gmail.com>
2020-01-03 11:43:33 -0500

    team page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import Nav from './components/Nav';
 import About from './components/About';
+import Team from './components/Team';
 import Dashboard from './components/Dashboard';
 import Stock from './components/Stock';
 
@@ -14,6 +15,7 @@ function App() {
       <Route path ='/' component={Nav} />
       <Route exact path='/' component={Dashboard} />
       <Route exact path='/about' component={About} />
+      <Route exact path='/team' component={Team} />
       <Route exact path='/signup' component={SignUp} />
       <Route exact path='/signin' component={SignIn} />
       <Route exact path='/stock/' component={Stock} />
