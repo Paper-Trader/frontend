0ca366f2485c7146e18540017b23abbc023133ce
Kristin Barr <kristinbarr@gmail.com>
2020-01-03 11:47:46 -0500

    about page updates

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import graphic from '../assets/about-graphic.svg';
-import { Header } from 'semantic-ui-react'
+import React from "react";
+import graphic from "../assets/about-graphic.svg";
+import { Header, Image, Segment, Grid, GridColumn } from "semantic-ui-react";
 
 function About() {
   return (
-    <div className="about-container">
-      <div>
-        <Header as='h2'>ABOUT</Header>
-        <img src={graphic} alt="graph illustration" />
-      </div>
-      <div className="right-content">
-        <h3>Test your trading strategies before you trade</h3>
-        <p>PaprTradr is a paper stock trading app. Get updates on constantly updates your portfolio based on market fluctuations via API calls.</p>
-        <p>Our simulated trading account allows you to test your strategies in real-time – without risking your capital.</p>
-      </div>
-    </div>
-  )
+    <Segment vertical className="about-container">
+      <Grid container stackable verticalAlign="middle" divided="vertically">
+        <Grid.Row width={16}>
+          <Header as="h2">ABOUT</Header>
+        </Grid.Row>
+        <Grid.Row columns={2}>
+          <Grid.Column width={8}>
+            <Image src={graphic} alt="graph illustration" />
+          </Grid.Column>
+          <Grid.Column width={8} className="right-content">
+            <Header as="h2">
+              Test your trading strategies before you trade
+            </Header>
+            <p>
+              PaprTradr is a paper stock trading app. Get updates on constantly
+              updates your portfolio based on market fluctuations via API calls.
+            </p>
+            <p>
+              Our simulated trading account allows you to test your strategies
+              in real-time – without risking your capital.
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+  );
 }
 export default About;
