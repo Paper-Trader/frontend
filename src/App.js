74d700dc6383f4ec4a1145f794c1bdb9e69d37d8
Kristin Barr <kristinbarr@gmail.com>
2020-01-03 12:00:01 -0500

    watchlist route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 // import Home from './components/Home';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
+import Dashboard from './components/Dashboard';
 import Nav from './components/Nav';
 import About from './components/About';
 import Team from './components/Team';
-import Dashboard from './components/Dashboard';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import WatchList from './components/WatchList';
 import Stock from './components/Stock';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       <Route exact path='/team' component={Team} />
       <Route exact path='/signup' component={SignUp} />
       <Route exact path='/signin' component={SignIn} />
+      <Route exact path='/watchlist' component={WatchList} />
       <Route exact path='/stock/' component={Stock} />
     </div>
   );
