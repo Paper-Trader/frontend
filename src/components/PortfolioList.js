008affd7d2a15605087adc1be05a60ec30c73158
Kristin Barr <kristinbarr@gmail.com>
2020-01-03 15:43:11 -0500

    added portfoliolist and dashboard updates

---
+++ src/components/PortfolioList.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { fetchAll } from "../actions";
+import { Header, Divider } from "semantic-ui-react";
+
+function Dashboard({ portfolio }) {
+  const [stocks, setStocks] = useState(portfolio.stocks);
+  console.log("stocks", stocks);
+
+  return (
+    <div className="portfoliolist">
+      {stocks.map(stock => (
+        <div key={stock.symbol}>
+          <Link to="/stock/">
+            <div className="stock">
+              <div className="stock-header">
+                <Header as="h2">{stock.symbol}</Header>
+                <Header as="h6">{stock.amount} shares</Header>
+              </div>
+              <Header as="h3">${stock.price}</Header>
+            </div>
+          </Link>
+          <Divider />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+const mapStateToProps = state => ({
+  portfolio: state.portfolio
+});
+
+export default connect(mapStateToProps, { fetchAll })(Dashboard);
