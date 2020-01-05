271b0fa96fee39caaf5fe521faf2b77dc50cd6e5
Kristin Barr <kristinbarr@gmail.com>
2020-01-04 20:54:24 -0500

    removed divider

--- src/components/watchlist/WatchList.js
+++ src/components/watchlist/WatchList.js
@@ -2,7 +2,7 @@ import React from "react";
 // import { connect } from "react-redux";
 import { LineChart, Line, XAxis, YAxis, Tooltip } from "recharts";
 import SideNav from "../nav/SideNav";
-import { Header, Divider } from "semantic-ui-react";
+import { Header } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 const data = [
@@ -22,42 +22,47 @@ export default function WatchList() {
       <SideNav />
       <div className="watchlist-content">
         <Header as="h5">Watchlist</Header>
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 20,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <XAxis dataKey="time" hide={true} domain={["priceMin", "priceMax"]} />
-          <YAxis />
-          <Tooltip />
-          <Line
-            connectNulls
-            type="monotone"
-            dataKey="price"
-            stroke="#8884d8"
-            fill="#8884d8"
-          />
-        </LineChart>
-        <div className="watchlist">
-          {data.map(stock => (
-            <div key={stock.symbol}>
-              <Link to="/stock/">
-                <div className="stock">
-                  <div className="stock-header">
-                    <Header as="h2">{stock.symbol}</Header>
+        <div className="watchlist-content-noheader">
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 0,
+              bottom: 0
+            }}
+          >
+            <XAxis
+              dataKey="time"
+              hide={true}
+              domain={["priceMin", "priceMax"]}
+            />
+            <YAxis />
+            <Tooltip />
+            <Line
+              connectNulls
+              type="monotone"
+              dataKey="price"
+              stroke="#8884d8"
+              fill="#8884d8"
+            />
+          </LineChart>
+          <div className="watchlist-list">
+            {data.map(stock => (
+              <div key={stock.symbol} className="stock-wrapper">
+                <Link to="/stock/">
+                  <div className="stock">
+                    <div className="stock-header">
+                      <Header as="h2">{stock.symbol}</Header>
+                    </div>
+                    <Header as="h3">${stock.price}</Header>
                   </div>
-                  <Header as="h3">${stock.price}</Header>
-                </div>
-              </Link>
-              <Divider />
-            </div>
-          ))}
+                </Link>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
