c5bdb2bdd5baf0a0c7232f6479924b64d767137f
BuildTools <arthurpisakhov1@gmail.com>
2020-01-07 19:29:37 -0500

    connected login to backend

--- src/components/login_register/SignIn.js
+++ src/components/login_register/SignIn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
   Form,
@@ -9,27 +9,66 @@ import {
   Segment
 } from "semantic-ui-react";
 import paper from "../../assets/paper-icon.svg";
+import { axiosWithAuth } from '../utils/axiosAuth';
 
 
-function SignIn() {
+function SignIn(props) {
+  const initialState = {
+    credentials: {
+      username: '',
+      password: '',
+    }
+  }
+
+  const [loginData, setLoginData] = useState(initialState)
+
+  const handleChange = e => {
+    setLoginData({
+      credentials: {
+        ...loginData.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+    console.log(loginData)
+  };
+
+  const login = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post('/auth/login', loginData.credentials)
+      .then(res => {
+        console.log(res)
+        localStorage.setItem('token', res.data.payload);
+        props.history.push('/dashboard')
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
       <Grid.Column style={{ maxWidth: 450 }}>
         <Header as="h2" color="teal" textAlign="center">
           <Image src={paper} /> Log-in to your account
         </Header>
-        <Form size="large">
+        <Form size="large" onSubmit={login}>
           <Segment stacked>
             <Form.Input
               fluid
               icon="user"
+              name="username"
               iconPosition="left"
-              placeholder="E-mail address"
+              value={loginData.credentials.username}
+              onChange={handleChange}
+              placeholder="Username"
+              type="text"
             />
             <Form.Input
               fluid
               icon="lock"
               iconPosition="left"
+              name="password"
+              value={loginData.credentials.password}
+              onChange={handleChange}
               placeholder="Password"
               type="password"
             />
