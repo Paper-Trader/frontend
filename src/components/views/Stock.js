bee35f583d1f97d0dac7cba0df3c79a660d20469
BuildTools <arthurpisakhov1@gmail.com>
2020-01-08 00:31:11 -0500

    fixed all warnings

--- src/components/views/Stock.js
+++ src/components/views/Stock.js
@@ -33,9 +33,6 @@ function Stock(props) {
   //   return [year, month, day].join("-");
   // }
 
-  let high, low;
-  let cash, perc;
-
   const green = { color: "green" },
     red = { color: "red" };
 
@@ -47,9 +44,9 @@ function Stock(props) {
         .get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${company}&interval=60min&apikey=${process.env.ALPHA_KEY}`)
         .then(data => {
           let newArr = [];
-          let time = Object.keys(data.data["Time Series (60min)"])[0]
+          let time = Object.keys(data.data["Time Series (60min)"])[0] // creates a var for most recent date, ex; '2020-01-08'
           for (const key in data.data["Time Series (60min)"]) {
-            if (key.includes(time.slice(0, (time.length/2)+1))) {
+            if (key.includes(time.slice(0, (time.length/2)+1))) { // checks if the key contains the variable above
               newArr.push({
                 ...data.data["Time Series (60min)"][key],
                 timestamp: key.split(" ")[1],
@@ -57,8 +54,8 @@ function Stock(props) {
               });
             }
           }
-          high = newArr[0]["2. high"];
-          low = newArr[0]["3. low"];
+          let high = newArr[0]["2. high"];
+          let low = newArr[0]["3. low"];
           newArr.forEach(obj => {
             if (obj["3. low"] < low) {
               low = obj["3. low"];
@@ -68,8 +65,8 @@ function Stock(props) {
             }
           });
           setGraphInfo(newArr.reverse());
-          cash = newArr[newArr.length - 1]["4. close"] - newArr[0].price;
-          perc = (cash / Math.abs(newArr[0].price)) * 100;
+          let cash = newArr[newArr.length - 1]["4. close"] - newArr[0].price;
+          let perc = (cash / Math.abs(newArr[0].price)) * 100;
           setLowHighCashPerc([low, high, cash.toFixed(2), perc.toFixed(2)]);
         });
       axios
@@ -80,7 +77,7 @@ function Stock(props) {
           setCompanyInfo(data.data.profile);
         });
     }
-  }, [lowHighCashPerc]);
+  }, [lowHighCashPerc, company]);
 
   return loading ? (
     <Loader type="BallTriangle" color="#00BFFF" height={100} width={100} />
