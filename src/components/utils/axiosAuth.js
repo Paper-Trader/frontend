147cf16f79803d122571a6e8332aaf8ab4a5e5ca
BuildTools <arthurpisakhov1@gmail.com>
2020-05-19 13:33:59 -0400

    fixed prod/dev server env

--- src/components/utils/axiosAuth.js
+++ src/components/utils/axiosAuth.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
 
+const devEndpoint = 'http://localhost:5000'
+const prodEndpoint = 'https://papr-tradr.herokuapp.com';
+
 export const axiosWithAuth = () => {
   const token = localStorage.getItem('token');
 
+
+
   return axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 
+      process.env.REACT_APP_CURR_ENV === "dev" 
+        ? devEndpoint 
+        : prodEndpoint,
     headers: {
       Authorization: token
     },
