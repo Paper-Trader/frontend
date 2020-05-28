8ae845d76a87308f697d30562a16f934b7a5a01f
BuildTools <arthurpisakhov1@gmail.com>
2020-05-28 15:56:09 -0400

    multiple responsive css changes

--- src/components/portfolio/PortfolioChart.js
+++ src/components/portfolio/PortfolioChart.js
@@ -1,34 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
-// const data = [
-//   {name: '9:00AM', value: 14000.12},
-//   {name: '10:00AM', value: 14300.24},
-//   {name: '11:00AM', value: 14100.78},
-//   {name: '12:00PM', value: 14500.45},
-//   {name: '1:00PM', value: 14600.57},
-//   {name: '2:00PM', value: 14700.78},
-//   {name: '3:00PM', value: 14900.57},
-//   {name: '4:00PM', value: 15009.36}
-// ];
+const data = [
+  {name: '9:00AM', value: 14000.12},
+  {name: '10:00AM', value: 14300.24},
+  {name: '11:00AM', value: 14100.78},
+  {name: '12:00PM', value: 14500.45},
+  {name: '1:00PM', value: 14600.57},
+  {name: '2:00PM', value: 14700.78},
+  {name: '3:00PM', value: 14900.57},
+  {name: '4:00PM', value: 15009.36}
+];
 
-function PortfolioChart({ dailyChange }) {
+function PortfolioChart() {
   return (
-    <div className="portfolio-background">
+    <div>
       {/* chart for dashboard page, x and y axis are hidden*/}
-      {/* <LineChart width={600} height={300} data={data}>
-        <Line 
-          type="monotone" 
-          dataKey="value" 
-          stroke={dailyChange.toString().includes("-") ? "#DC4A7F" : "#00D1C5"}
-          strokeWidth={3}
-          dot={false}
-        />
-        <YAxis hide={true} domain={['dataMin - 1500', 'dataMax + 1500']} />
-        <XAxis dataKey="name" hide={true} stroke="#8884d8" />
-        <Tooltip />
-      </LineChart> */}
+      <ResponsiveContainer width={420} height={300}>
+        <LineChart data={data}>
+          <Line 
+            type="monotone" 
+            dataKey="value" 
+            stroke="#00D1C5"
+            strokeWidth={3}
+            dot={false}
+          />
+          <YAxis hide={true} domain={['dataMin - 1500', 'dataMax + 1500']} />
+          <XAxis dataKey="name" hide={true} stroke="#8884d8" />
+          <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 }
